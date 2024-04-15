b70af20d07644606a4074bedf8ac9deb9c98e645
Marlon Abella <mardavab@gmail.com>
2024-04-15 11:21:51 -0500

    Lo que hicimos el viernes

---
+++ assets/js/scripts/login.js
@@ -0,0 +1,16 @@
+function login() {
+    var username = $('#username').val();
+    var password = $('#password').val();
+    
+
+    if (!username || !password) 
+        return M.toast({
+            html: '<span class="orange-text ">Asegurese que todos los campos esten llenos</span>',
+            classes: 'rounded  orange lighten-5 '
+        })
+    
+        return Swal.fire({
+            icon: (username == "admin" && password == "1234") ? 'success': 'error' ,
+            title: (username == "admin" && password == "1234") ? 'Incio sesion el admin':'no Inicio sesion el admin'
+        })
+}
